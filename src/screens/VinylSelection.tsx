7189aed8a4218d5cc666689f9c2010fb8eb36183
Nay Mello <naymellodev@gmail.com>
2022-03-20 20:15:19 -0300

    feat: add interfaces

--- src/screens/VinylSelection.tsx
+++ src/screens/VinylSelection.tsx
@@ -1,10 +1,23 @@
 import { View, Text } from 'react-native'
 import React, { FC } from 'react'
 
+import { IVinyl } from '../common/types'
 import { useRequest } from '../hooks/useRequest'
 
+interface IGistResponse {
+  files: IFiles
+}
+
+interface IFiles {
+  [key: string]: IVinylJSON
+}
+
+interface IVinylJSON {
+  content: string
+}
+
 const VinylSelection: FC = () => {
-  const { response: gist } = useRequest(
+  const { response: gist } = useRequest<IGistResponse>(
     'https://api.github.com/gists/3648d4454d0c68d836fac03386c17be1'
   )
 
