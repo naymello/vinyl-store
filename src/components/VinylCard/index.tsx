a6cef502d6123ee9cc0a090a371a1ec5282959fe
Nay Mello <naymellodev@gmail.com>
2022-03-20 18:21:28 -0300

    feat: create VinylCard component

---
+++ src/components/VinylCard/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+
+import { IVinyl } from '../../common/types'
+
+const VinylCard: React.FC<IVinyl> = (props) => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default VinylCard
