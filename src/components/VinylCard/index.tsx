07d6a8c7197f764b174756550ad43d8bc90480e4
Nay Mello <naymellodev@gmail.com>
2022-03-20 19:57:24 -0300

    feat: add information to card component

--- src/components/VinylCard/index.tsx
+++ src/components/VinylCard/index.tsx
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 import { IVinyl } from '../../common/types'
 
-const VinylCard: React.FC<IVinyl> = (props) => {
+const VinylCard: React.FC<IVinyl> = ({ title, author, price, cover }) => {
   return (
     <View>
-      <Text></Text>
+      <Image
+        source={{ uri: cover }}
+        style={{
+          width: 150,
+          height: 150,
+        }}
+      ></Image>
+      <View>
+        <Text>{title}</Text>
+        <Text>{author}</Text>
+        <Text>$ {price}</Text>
+      </View>
     </View>
   )
 }
