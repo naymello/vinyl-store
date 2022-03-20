906db95cc1978d042ce06f67c734d9676719071a
Nay Mello <naymellodev@gmail.com>
2022-03-20 12:47:35 -0300

    feat: add Vinyl screen

---
+++ src/screens/Vinyl.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Vinyl = () => {
+  return (
+    <View>
+      <Text>Vinyl</Text>
+    </View>
+  )
+}
+
+export default Vinyl
