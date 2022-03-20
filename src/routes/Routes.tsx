3dec7f8fb54b52044cf7e45d10e1e06f87ca024c
Nay Mello <naymellodev@gmail.com>
2022-03-20 13:23:10 -0300

    feat: add Routes component

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react'
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+import VinylSelection from '../screens/VinylSelection'
+import VinylDetails from '../screens/VinylDetails'
+import Bag from '../screens/Bag'
+
+const Stack = createNativeStackNavigator()
+
+const Routes: FC = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="VinylSelection" component={VinylSelection} />
+      <Stack.Screen name="VinylDetails" component={VinylDetails} />
+      <Stack.Screen name="Bag" component={Bag} />
+    </Stack.Navigator>
+  </NavigationContainer>
+)
+
+export default Routes
