8aa83ccc24d7521286d7216d46cdc334f8deff26
Nay Mello <naymellodev@gmail.com>
2022-03-20 20:11:56 -0300

    feat: get gist through GitHub API

--- src/screens/VinylSelection.tsx
+++ src/screens/VinylSelection.tsx
@@ -1,7 +1,13 @@
 import { View, Text } from 'react-native'
 import React, { FC } from 'react'
 
+import { useRequest } from '../hooks/useRequest'
+
 const VinylSelection: FC = () => {
+  const { response: gist } = useRequest(
+    'https://api.github.com/gists/3648d4454d0c68d836fac03386c17be1'
+  )
+
   return (
     <View>
       <Text>VinylSelection</Text>
