06de48148c316e461cfd3f8c90e971b0d3ffce44
Nay Mello <naymellodev@gmail.com>
2022-03-20 12:47:46 -0300

    feat: add VinylSelection screen

---
+++ src/screens/VinylSelection.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React, { FC } from 'react'
+
+const VinylSelection: FC = () => {
+  return (
+    <View>
+      <Text>VinylSelection</Text>
+    </View>
+  )
+}
+
+export default VinylSelection
