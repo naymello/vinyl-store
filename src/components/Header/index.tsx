94f106038261dd0743b57980087c72727263d79b
Nay Mello <naymellodev@gmail.com>
2022-03-22 17:30:21 -0300

    feat: make header dynamic between screens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,13 +5,31 @@ import { Container, SafeAreaContainer } from './styles'
 import Logo from '../../assets/Logo'
 import BagIcon from '../../assets/BagIcon'
 
-const Header: React.FC<NativeStackHeaderProps> = () => {
+interface IHeaderContent {
+  [key: string]: JSX.Element
+}
+
+const VinylSelectionContent: React.FC = () => {
+  return (
+    <>
+      <Logo />
+      <BagIcon />
+    </>
+  )
+}
+
+const Header: React.FC<NativeStackHeaderProps> = ({ route }) => {
+  const getHeaderContent = (routeName: string): JSX.Element => {
+    const headerContent: IHeaderContent = {
+      VinylSelection: <VinylSelectionContent />,
+    }
+
+    return headerContent[routeName]
+  }
+
   return (
     <SafeAreaContainer>
-      <Container>
-        <Logo />
-        <BagIcon />
-      </Container>
+      <Container>{getHeaderContent(route.name)}</Container>
     </SafeAreaContainer>
   )
 }
