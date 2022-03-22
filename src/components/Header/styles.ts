1c8228b8478fa3dedc6741251cb69a852f49c45b
Nay Mello <naymellodev@gmail.com>
2022-03-22 14:34:43 -0300

    style: change header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -4,6 +4,7 @@ export const Container = styled.View`
   width: 100%;
   height: 120px;
   background-color: ${({ theme }) => theme.colors.accent};
-  justify-content: flex-end;
-  padding: 15px 20px;
+  flex-direction: row;
+  align-items: flex-end;
+  padding: 15px;
 `
