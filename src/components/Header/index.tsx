121aa9fe6bf0cf650493f7655022a613c94709e7
Nay Mello <naymellodev@gmail.com>
2022-03-22 14:46:05 -0300

    feat: add BagIcon to the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,12 +3,14 @@ import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 import { Container, SafeAreaContainer } from './styles'
 
 import Logo from '../../assets/Logo'
+import BagIcon from '../../assets/BagIcon'
 
 const Header: React.FC<NativeStackHeaderProps> = () => {
   return (
     <SafeAreaContainer>
       <Container>
         <Logo />
+        <BagIcon />
       </Container>
     </SafeAreaContainer>
   )
