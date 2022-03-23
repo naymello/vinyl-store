f0c60f43876128624aa3b204829b3aa801d99a53
Nay Mello <naymellodev@gmail.com>
2022-03-23 09:49:01 -0300

    feat: add BagNavigationIcon component

---
+++ src/components/BagNavigationIcon/index.tsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react'
+
+import { IBagContext } from '../../common/types'
+import { BagContext } from '../../contexts/BagContext'
+import NavigationIcon from '../NavigationIcon'
+
+import { BagCounter } from './styles'
+import BagIcon from '../../assets/BagIcon'
+
+interface Props {
+  navigate: any
+}
+
+const BagNavigationIcon: React.FC<Props> = ({ navigate }) => {
+  const { bagTotalQuantity } = useContext(BagContext) as IBagContext
+
+  return (
+    <NavigationIcon navigate={navigate} icon={<BagIcon />}>
+      {bagTotalQuantity && <BagCounter>{bagTotalQuantity}</BagCounter>}
+    </NavigationIcon>
+  )
+}
+
+export default BagNavigationIcon
