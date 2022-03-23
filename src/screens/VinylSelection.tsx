792827d9a70826e010f173bf599dc1a664d661f0
Nay Mello <naymellodev@gmail.com>
2022-03-23 08:08:51 -0300

    feat: add type to component props

--- src/screens/VinylSelection.tsx
+++ src/screens/VinylSelection.tsx
@@ -1,11 +1,20 @@
 import { ScrollView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
 import { IVinyl } from '../common/types'
 import { useRequest } from '../hooks/useRequest'
 
 import VinylCard from '../components/VinylCard'
 import { Container } from '../components/Container/styles'
+
+type RootStackParamList = {
+  VinylSelection: undefined
+  Bag: undefined
+  VinylDetails: { id: number } | undefined
+}
+
+type Props = NativeStackScreenProps<RootStackParamList, 'VinylSelection'>
 
 interface IGistResponse {
   files: IFiles
@@ -19,7 +28,7 @@ interface IVinylJSON {
   content: string
 }
 
-const VinylSelection: React.FC = () => {
+const VinylSelection: React.FC<Props> = ({ navigation }) => {
   const { response: gist } = useRequest<IGistResponse>(
     'https://api.github.com/gists/3648d4454d0c68d836fac03386c17be1'
   )
