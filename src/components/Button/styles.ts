a711344b8a6a56b40d978f3d4baa9bc60f211563
Nay Mello <naymellodev@gmail.com>
2022-03-23 10:50:43 -0300

    style: add styles to Button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native'
+
+export const Touchable = styled.TouchableOpacity`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.accent};
+  flex-direction: row;
+  justify-content: center;
+  padding: 20px;
+  margin: 10px 0;
+`
+
+export const ButtonText = styled.Text`
+  color: ${({ theme }) => theme.colors.accentText};
+  font-size: 20px;
+  font-weight: bold;
+`
