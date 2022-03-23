d8a9f9c8a539c969f845d7aee064cd446ab00018
Nay Mello <naymellodev@gmail.com>
2022-03-23 11:19:03 -0300

    feat: add type to VinylDetails props

--- src/screens/VinylDetails.tsx
+++ src/screens/VinylDetails.tsx
@@ -1,6 +1,12 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+import { RootStackParamList } from '../common/types'
+
 import VinylInformation from '../components/VinyInformation'
 
-const VinylDetails: React.FC = () => {
+type Props = NativeStackScreenProps<RootStackParamList, 'VinylSelection'>
+
+const VinylDetails: React.FC<Props> = ({ route }) => {
   return (
     <>
       <VinylInformation />
