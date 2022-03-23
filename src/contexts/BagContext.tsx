6857339e64b00541f28a34253cdde739fcafde3a
Nay Mello <naymellodev@gmail.com>
2022-03-22 22:45:09 -0300

    feat: export BagContext

--- src/contexts/BagContext.tsx
+++ src/contexts/BagContext.tsx
@@ -2,7 +2,7 @@ import { useState, createContext } from 'react'
 
 import { IVinyl, IBagContext } from '../common/types'
 
-const BagContext = createContext<IBagContext | null>(null)
+export const BagContext = createContext<IBagContext | null>(null)
 
 const BagContextProvider: React.FC = ({ children }) => {
   const [vinyl, setVinyl] = useState<IVinyl[]>([])
