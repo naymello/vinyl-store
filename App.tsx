7c8f29e0851b861dd0ad176f055cf43f8792bec2
Nay Mello <naymellodev@gmail.com>
2022-03-22 21:47:11 -0300

    refactor: change routes.tsx case

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components'
 
-import Routes from './src/routes/Routes'
+import Routes from './src/routes/routes'
 import mainTheme from './src/themes/mainTheme'
 
 export default function App() {
