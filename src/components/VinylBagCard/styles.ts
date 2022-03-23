2935be17cd578b4e38046e0250388d16a5d3f0ab
Nay Mello <naymellodev@gmail.com>
2022-03-23 01:07:10 -0300

    styles: add styles to VinylBagCard

---
+++ src/components/VinylBagCard/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native'
+
+export const Wrapper = styled.View`
+  flex-direction: row;
+  margin: 15px 0;
+`
+
+export const Information = styled.View`
+  padding: 10px 5px;
+`
+
+export const Cover = styled.Image`
+  width: 120px;
+  height: 120px;
+  margin-right: 15px;
+`
+
+export const Title = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  width: 200px;
+`
+
+export const Price = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: auto;
+`
