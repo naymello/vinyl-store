ebb0515c51743ac2bcd682f4f5101d4aa3968efb
Nay Mello <naymellodev@gmail.com>
2022-03-23 09:48:39 -0300

    feat: add prop children to NavigationIcon

--- src/components/NavigationIcon/index.tsx
+++ src/components/NavigationIcon/index.tsx
@@ -5,13 +5,14 @@ interface Props {
   icon: JSX.Element
 }
 
-const NavigationIcon: React.FC<Props> = ({ navigate, icon }) => {
+const NavigationIcon: React.FC<Props> = ({ navigate, icon, children }) => {
   return (
     <TouchableOpacity
       onPress={navigate}
       hitSlop={{ top: 50, bottom: 50, left: 50, right: 50 }}
     >
       {icon}
+      {children}
     </TouchableOpacity>
   )
 }
