544697eb8cdf31ced109dbf93bda44e4211a4223
Nay Mello <naymellodev@gmail.com>
2022-03-23 01:08:20 -0300

    feat: add VinylBagCard component on Bag screen

--- src/screens/Bag.tsx
+++ src/screens/Bag.tsx
@@ -5,13 +5,18 @@ import { BagContext } from '../contexts/BagContext'
 import { IBagContext } from '../common/types'
 
 import { Container } from '../components/Container/styles'
+import VinylBagCard from '../components/VinylBagCard'
 
 const Bag: React.FC = () => {
   const { vinylInBag } = useContext(BagContext) as IBagContext
 
   return (
     <ScrollView>
-      <Container></Container>
+      <Container>
+        {vinylInBag.map((vinyl) => (
+          <VinylBagCard {...vinyl} key={vinyl.id} />
+        ))}
+      </Container>
     </ScrollView>
   )
 }
