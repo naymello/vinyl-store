be91dafa44ef28849288b2d828e0f5bf6ed0b053
Nay Mello <naymellodev@gmail.com>
2022-03-22 22:34:31 -0300

    feat: add bag context provider to App

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import { ThemeProvider } from 'styled-components'
 
 import Routes from './src/routes/routes'
 import mainTheme from './src/themes/mainTheme'
+import BagContextProvider from './src/contexts/BagContext'
 
 export default function App() {
   return (
-    <ThemeProvider theme={mainTheme}>
-      <Routes />
-    </ThemeProvider>
+    <BagContextProvider>
+      <ThemeProvider theme={mainTheme}>
+        <Routes />
+      </ThemeProvider>
+    </BagContextProvider>
   )
 }
