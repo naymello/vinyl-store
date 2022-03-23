14009dae01c1981c0e576d4bdaa2bad0696368ba
Nay Mello <naymellodev@gmail.com>
2022-03-22 23:05:59 -0300

    feat: apply context to Bag screen

--- src/screens/Bag.tsx
+++ src/screens/Bag.tsx
@@ -1,8 +1,14 @@
+import { useContext } from 'react'
 import { ScrollView } from 'react-native'
+
+import { BagContext } from '../contexts/BagContext'
+import { IBagContext } from '../common/types'
 
 import { Container } from '../components/Container/styles'
 
 const Bag: React.FC = () => {
+  const { vinyl } = useContext(BagContext) as IBagContext
+
   return (
     <ScrollView>
       <Container></Container>
