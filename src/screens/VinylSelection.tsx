c4ba36eb847012946227576b951fbfcd5c113b91
Nay Mello <naymellodev@gmail.com>
2022-03-23 11:54:20 -0300

    refactor: get vinyl from context

--- src/screens/VinylSelection.tsx
+++ src/screens/VinylSelection.tsx
@@ -1,37 +1,25 @@
+import { useContext } from 'react'
 import { ScrollView } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-import { IVinyl, RootStackParamList } from '../common/types'
-import { useRequest } from '../hooks/useRequest'
+import { IVinylContext, RootStackParamList } from '../common/types'
 
 import VinylCard from '../components/VinylCard'
 import { Container } from '../components/Container/styles'
+import { VinylContext } from '../contexts/VinylContext'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'VinylSelection'>
 
-interface IGistResponse {
-  files: {
-    [key: string]: {
-      content: string
-    }
-  }
-}
-
 const VinylSelection: React.FC<Props> = ({ navigation }) => {
-  const { response: gist } = useRequest<IGistResponse>(
-    'https://api.github.com/gists/3648d4454d0c68d836fac03386c17be1'
-  )
-
-  const vinylJSON = gist?.files['vinyl.json'].content
-  const vinyl: IVinyl[] = vinylJSON ? JSON.parse(vinylJSON) : []
+  const { vinyl } = useContext(VinylContext) as IVinylContext
 
   return (
     <>
       <StatusBar />
       <ScrollView>
         <Container>
-          {vinyl?.map((vinyl) => (
+          {vinyl.map((vinyl) => (
             <VinylCard {...vinyl} key={vinyl.id} navigation={navigation} />
           ))}
         </Container>
