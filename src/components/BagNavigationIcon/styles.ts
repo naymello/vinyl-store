64e80b6f8eca85784b367778c5aca8a8dad31fd6
Nay Mello <naymellodev@gmail.com>
2022-03-23 10:18:03 -0300

    style: change position depending on characters

--- src/components/BagNavigationIcon/styles.ts
+++ src/components/BagNavigationIcon/styles.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components/native'
 export const BagCounter = styled.Text`
   position: absolute;
   top: 10px;
-  left: 10px;
+  left: ${({ children }) =>
+    (children?.valueOf() as number) < 10 ? '10px' : '8px'};
   font-size: 10px;
   font-weight: bold;
   color: ${({ theme }) => theme.colors.accentText};
