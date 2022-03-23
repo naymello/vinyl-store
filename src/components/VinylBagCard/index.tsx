8c4498d9306f73327b6f432bd2cccf6bcc9ec3f2
Nay Mello <naymellodev@gmail.com>
2022-03-23 02:08:25 -0300

    feat: add DeleteButton to VinylBagCard

--- src/components/VinylBagCard/index.tsx
+++ src/components/VinylBagCard/index.tsx
@@ -1,10 +1,18 @@
 import { IVinyl } from '../../common/types'
 
-import { Cover, Title, Wrapper, Price, Information } from './styles'
+import {
+  Cover,
+  Title,
+  Wrapper,
+  Price,
+  Information,
+  DeleteButton,
+} from './styles'
 
 const VinylBagCard: React.FC<IVinyl> = ({ title, price, cover }) => {
   return (
     <Wrapper>
+      <DeleteButton />
       <Cover source={{ uri: cover }}></Cover>
       <Information>
         <Title>{title}</Title>
