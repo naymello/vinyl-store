461010b39952a7b551f09640a514b7f0dc774212
Nay Mello <naymellodev@gmail.com>
2022-03-23 11:50:44 -0300

    feat: add ThemeProvider in ContextProviders

--- src/contexts/ContextProviders.tsx
+++ src/contexts/ContextProviders.tsx
@@ -1,10 +1,15 @@
 import VinylContextProvider from './VinylContext'
 import BagContextProvider from './BagContext'
+import { ThemeProvider } from 'styled-components'
+
+import mainTheme from '../themes/mainTheme'
 
 const ContextProviders: React.FC = ({ children }) => {
   return (
     <VinylContextProvider>
-      <BagContextProvider>{children}</BagContextProvider>
+      <BagContextProvider>
+        <ThemeProvider theme={mainTheme}>{children}</ThemeProvider>
+      </BagContextProvider>
     </VinylContextProvider>
   )
 }
