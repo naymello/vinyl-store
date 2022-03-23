d05ccce4379a982a17c165f77c228966b45272c2
Nay Mello <naymellodev@gmail.com>
2022-03-23 11:26:45 -0300

    feat: get addVinyl method from the context

--- src/screens/VinylDetails.tsx
+++ src/screens/VinylDetails.tsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-import { RootStackParamList } from '../common/types'
+import { IBagContext, RootStackParamList } from '../common/types'
 
+import { BagContext } from '../contexts/BagContext'
 import VinylInformation from '../components/VinyInformation'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'VinylSelection'>
 
 const VinylDetails: React.FC<Props> = ({ route }) => {
+  const { addVinyl } = useContext(BagContext) as IBagContext
+
   return (
     <>
       <VinylInformation />
