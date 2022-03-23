8eb8f39a96a87c2a7b4de6444660277c5ab24b53
Nay Mello <naymellodev@gmail.com>
2022-03-22 21:08:51 -0300

    feat: add hitSlop to bag icon

--- src/components/BagButton/index.tsx
+++ src/components/BagButton/index.tsx
@@ -8,7 +8,10 @@ interface Props {
 
 const BagButton: React.FC<Props> = ({ navigation }) => {
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('Bag')}>
+    <TouchableOpacity
+      onPress={() => navigation.navigate('Bag')}
+      hitSlop={{ top: 50, bottom: 50, left: 50, right: 50 }}
+    >
       <BagIcon />
     </TouchableOpacity>
   )
