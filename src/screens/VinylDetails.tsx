07d8d42049eecb762b1cfb58c2eff614ee7c8039
Nay Mello <naymellodev@gmail.com>
2022-03-23 13:07:14 -0300

    feat: add vinyl context to VinylDetails

--- src/screens/VinylDetails.tsx
+++ src/screens/VinylDetails.tsx
@@ -1,19 +1,24 @@
 import { useContext } from 'react'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 
-import { IBagContext, RootStackParamList } from '../common/types'
+import { IBagContext, IVinylContext, RootStackParamList } from '../common/types'
 
 import { BagContext } from '../contexts/BagContext'
+import { VinylContext } from '../contexts/VinylContext'
+
 import VinylInformation from '../components/VinyInformation'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'VinylSelection'>
 
 const VinylDetails: React.FC<Props> = ({ route }) => {
+  const { getVinylById } = useContext(VinylContext) as IVinylContext
   const { addVinyl } = useContext(BagContext) as IBagContext
+
+  const vinyl = getVinylById(route.params?.id as number)
 
   return (
     <>
-      <VinylInformation />
+      <VinylInformation vinyl={vinyl} />
     </>
   )
 }
