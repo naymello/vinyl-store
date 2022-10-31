3a31a6645414ba6c0fafa3857bf92c456de20535
Nay Mello <naymellodev@gmail.com>
2022-10-31 17:32:54 -0300

    chore: set up React Query

--- src/contexts/ContextProviders.tsx
+++ src/contexts/ContextProviders.tsx
@@ -1,16 +1,22 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 import VinylContextProvider from './VinylContext'
 import BagContextProvider from './BagContext'
 import { ThemeProvider } from 'styled-components'
 
 import mainTheme from '../themes/mainTheme'
 
+const queryClient = new QueryClient()
+
 const ContextProviders: React.FC = ({ children }) => {
   return (
-    <VinylContextProvider>
-      <BagContextProvider>
-        <ThemeProvider theme={mainTheme}>{children}</ThemeProvider>
-      </BagContextProvider>
-    </VinylContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <VinylContextProvider>
+        <BagContextProvider>
+          <ThemeProvider theme={mainTheme}>{children}</ThemeProvider>
+        </BagContextProvider>
+      </VinylContextProvider>
+    </QueryClientProvider>
   )
 }
 
