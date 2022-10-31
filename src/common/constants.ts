6834612f8209b392687bf6d402a91221870bc935
Nay Mello <naymellodev@gmail.com>
2022-10-31 17:32:21 -0300

    chore: add Spotify related constants

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -10,3 +10,8 @@ export const screenDimensions = {
 
 export const gitHubGistsEndpoint =
   'https://api.github.com/gists/3648d4454d0c68d836fac03386c17be1'
+
+export const spotifyEndpoint = 'https://api.spotify.com/v1/'
+
+export const spotifyAuthToken =
+  'Bearer BQARctldrnUPU_2ZRNZ_ntAB4i6q0Ubhx9b6qCQKXPOqBgq5cWIa6GU43FMNhWUeLcvnZVk07QOCX-AN9O9CBO6YsOhklgU1kiNisQOCrs64mBJUwqaYZS_OycnpK7HZXJg5GmOyZdUatswhj4eSgwyQ1BJkxXbqt6wM6wiWPmct'
