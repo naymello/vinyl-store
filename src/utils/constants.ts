3d6bda838ba013d16eb56e0d6b1ca44b9d74011d
Nay Mello <naymellodev@gmail.com>
2022-10-31 17:50:32 -0300

    fix: remove slash from endpoint

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -11,7 +11,7 @@ export const screenDimensions = {
 export const gitHubGistsEndpoint =
   'https://api.github.com/gists/3648d4454d0c68d836fac03386c17be1'
 
-export const spotifyEndpoint = 'https://api.spotify.com/v1/'
+export const spotifyEndpoint = 'https://api.spotify.com/v1'
 
 export const spotifyAuthToken =
-  'Bearer BQARctldrnUPU_2ZRNZ_ntAB4i6q0Ubhx9b6qCQKXPOqBgq5cWIa6GU43FMNhWUeLcvnZVk07QOCX-AN9O9CBO6YsOhklgU1kiNisQOCrs64mBJUwqaYZS_OycnpK7HZXJg5GmOyZdUatswhj4eSgwyQ1BJkxXbqt6wM6wiWPmct'
+  'Bearer BQBkgtN5iUEw1r5qx7-ptqNDhSVmHkRmukYo6k2ytYEZ9D9yveJZfSV5BhwDqmpxV8XYQTgOuzWoM4DGOFM7AKpFa4Reh8vVZAZJNMTTT_KeHEThMZruhDFyUP0LpUibAhh77wisFf4ehQ-a6WM6XO_dryGSqDqcAV2c8lrh6hCm'
