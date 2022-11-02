f01f7b545fc8a509b4a4c34a5773d4c147aade5d
Nay Mello <naymellodev@gmail.com>
2022-11-01 21:51:13 -0300

    feat: use imported type instead of inline

--- src/contexts/AlbumsContext.tsx
+++ src/contexts/AlbumsContext.tsx
@@ -2,9 +2,9 @@ import { createContext } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { getPlaylist } from '../services/getPlaylist'
 import { getAlbumsFromPlaylist } from '../utils/getAlbumsFromPlaylist'
-import { Album } from '../common/types'
+import { AlbumsData } from '../common/types'
 
-export const AlbumsContext = createContext<{ albums: Album[] } | null>(null)
+export const AlbumsContext = createContext<AlbumsData | null>(null)
 
 const AlbumsContextProvider: React.FC = ({ children }) => {
   const { data: playlist } = useQuery(['playlist'], () =>
