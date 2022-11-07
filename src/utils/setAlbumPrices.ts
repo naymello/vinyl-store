1405ed87aecb505f60ee9626171c678059e2a921
Nay Mello <naymellodev@gmail.com>
2022-11-06 21:24:16 -0300

    feat: change price setting rule

--- src/utils/setAlbumPrices.ts
+++ src/utils/setAlbumPrices.ts
@@ -3,7 +3,7 @@ import { Album } from '../common/types'
 export const setAlbumPrices = (albums: Album[]): Album[] => {
   const albumsPriced = albums.map((album) => ({
     ...album,
-    price: album.tracks?.length! * 5 + 9.99,
+    price: album.tracks?.length! + 19.99,
   }))
 
   return albumsPriced
